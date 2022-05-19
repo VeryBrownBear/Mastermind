533827942b0fd37af393a9727542f232b33aafa5
VeryBrownBear <45986107+VeryBrownBear@users.noreply.github.com>
2022-05-18 20:37:15 -0500

    Setup for timing capabilities

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from game import Game
+import config
 
-# If the code length is larger than the number of colors then allow_duplicates has to be true
-# If it is less than or equal to then allow_duplicates can be either
-# CODE LENGTH, NUMBER OF COLORS, DUPLICATES?, NUMBER OF GUESSES, HUMAN_PLAYING?
-game = Game(5, 8, True, 10, False)
-game.play()
+trial_number = 0
+max_trials = 25
+total_time = 0
+total_guesses = 0
+while trial_number < max_trials or config.global_game_over:
+# CODE LENGTH, NUMBER OF COLORS, DUPLICATES?, NUMBER OF GUESSES, HUMAN_PLAYING?, ALGORITHM (0 for GENETIC, 1 FOR KNUTH)
+    game = Game(4, 6, True, 10, False, 1)
+    timing = game.play()
+    total_time += timing[0]
+    total_guesses += timing[1]
+    trial_number += 1
+print(total_time / max_trials)
+print(total_guesses / max_trials)
